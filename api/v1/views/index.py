6eb837461ae7ea2527c7dc73a7268f84e9b2cd94
EvaMicich <6366@holbertonstudents.com>
2023-08-24 05:53:56 -0700

    pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,8 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
+    """returst status:OK in json"""
     return jsonify({"status": "OK"})
