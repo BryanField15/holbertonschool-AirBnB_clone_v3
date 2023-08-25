1c2b1beb2e18b873ba9cb550d757bf155c5b15b6
EvaMicich <6366@holbertonstudents.com>
2023-08-24 23:03:45 -0700

    stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,29 @@
 """ returns json ok status"""
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """returst status:OK in json"""
+    """returns status:OK in json"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """returns count for classes in json format"""
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+        })
