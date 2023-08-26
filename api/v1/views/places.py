ee8837a806b3231ec7965894453785a05e61ce0c
EvaMicich <6366@holbertonstudents.com>
2023-08-26 04:35:17 -0700

    fix POST

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -42,14 +42,23 @@ def delete_place(place_id):
 @app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
 def create_place_by_city(city_id):
     """Creates a Place"""
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
     data = request.get_json()
     if not data:
         return jsonify({"error": "Not a JSON"}), 400
+    if 'user_id' not in data:
+        return jsonify({"error": "Missing user_id"}), 400
+    user = storage.get(User, data['user_id'])
+    if not user:
+        abort(404)
     if 'name' not in data:
         return jsonify({"error": "Missing name"}), 400
+    data['city_id'] = city_id
     new_place = Place(**data)
-    new_place.city_id = city_id
-    new_place.save()
+    storage.new(new_place)
+    storage.save()
     return jsonify(new_place.to_dict()), 201
 
 
