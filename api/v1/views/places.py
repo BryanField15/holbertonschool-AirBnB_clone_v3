9e0f514bc2fb1edda041ee6829bd7be0510e844e
EvaMicich <6366@holbertonstudents.com>
2023-08-25 23:13:31 -0700

    added city class

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -2,21 +2,23 @@
 """View for Place objects that handles all default RESTFul API actions"""
 from flask import Flask, jsonify, abort, request
 from models.place import Place
+from models.city import City
+from models.user import User
 from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'], 
+@app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
 def get_places_by_city(city_id):
-    """Retrieves the list of all Place objects"""
-    city = storage.get(Place, city_id)
-    if not city:
+    """Retrieves the list of all Place objects in a city"""
+    city = storage.get(City, city_id)
+    if not city_dict:
         abort(404)
     return jsonify([place.to_dict() for place in city.places])
 
 
-@app_views.route('/places/<place_id>', methods=['GET'], 
+@app_views.route('/places/<place_id>', methods=['GET'],
                  strict_slashes=False)
 def get_place(place_id):
     """Retrieves a Place object"""
@@ -26,7 +28,7 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'], 
+@app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
@@ -38,7 +40,7 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'], 
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def create_place_by_city(city_id):
     """Creates a Place"""
@@ -66,7 +68,8 @@ def create_place_by_city(city_id):
 #    return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_place(place_id):
     """Updates a Place object"""
     place = storage.get(Place, place_id)
@@ -76,10 +79,10 @@ def update_place(place_id):
     if not data:
         return jsonify({"error": "Not a JSON"}), 400
     for key, value in data.items():
-        if key not in ['id', 
-                       'user_id', 
-                       'city_id', 
-                       'created_at', 
+        if key not in ['id',
+                       'user_id',
+                       'city_id',
+                       'created_at',
                        'updated_at']:
             setattr(place, key, value)
     place.save()
